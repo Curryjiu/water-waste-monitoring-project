a6e948e029f50d7cb9a6c2deec7323a243811e90
sunzhenqiang <enderman19980125@outlook.com>
2025-08-05 16:07:12 +0800

    Add flask.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world.")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "hello world Curryjiu"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
