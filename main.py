da304e902883dc4f387ee15ff76bf2a731b0669a
sunzhenqiang <enderman19980125@outlook.com>
2025-08-05 16:22:21 +0800

    Add home page.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "hello world Curryjiu"
+    return render_template("main.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True, port=9988)
